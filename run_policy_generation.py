d7ebc1975baa88f371d836f73c2a43dacbbc6235
Colin Simon <s81601@htw-dresden.de>
2022-07-22 17:52:31 +0200

    changed back to medmnist

--- run_policy_generation.py
+++ run_policy_generation.py
@@ -4,14 +4,6 @@
 # import datetime
 # import keras
 
-# path = "pathmnist.npz"
-# x_train = None
-# y_train = None
-# with np.load(path) as data:
-#     lst = data.files
-#     x_train = data["train_images"]
-#     y_train = data["train_labels"]
-
 
 # now = datetime.datetime.now()
 # EXPERIMENT_NAME = f"{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}"
@@ -46,13 +38,23 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+# (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+# path = "fashion_mnist"
+# stacked_img = np.stack((x_train,) * 3, axis=-1)
+
+path = "pathmnist.npz"
+x_train = None
+y_train = None
+with np.load(path) as data:
+    lst = data.files
+    x_train = data["train_images"]
+    y_train = data["train_labels"]
 
 now = datetime.datetime.now()
 EXPERIMENT_NAME = f"{now.month:02}-{now.day:02}_{now.hour:02}-{now.minute:02}"
 # child_epochs set to 10 for a quick run, but it should be >=50 for a proper analysis
 
-path = "fashion_mnist"
+
 nb_path = "logs_" + path + EXPERIMENT_NAME
 Path(nb_path).mkdir(parents=True, exist_ok=True)
 # child_epochs set to 10 for a quick run, but it should be >=50 for a proper analysis
@@ -72,9 +74,8 @@ my_config = {
 # X_train.shape -> (N, M, M, 3)
 # y_train.shape -> (N)
 
-stacked_img = np.stack((x_train,) * 3, axis=-1)
-print(stacked_img.shape)
-deepaug = DeepAugment(images=stacked_img, labels=y_train, config=my_config)
+
+deepaug = DeepAugment(images=x_train, labels=y_train, config=my_config)
 
 best_policies = deepaug.optimize(100)
 
